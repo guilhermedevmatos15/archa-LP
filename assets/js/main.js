62df9566916bbdbe3d7dc1e782a8af0849a193a1
guilhermedevmatos15 <devguilhermeferreira@gmail.com>
2023-04-14 15:00:30 -0300

    feat: interatividade nos abouts

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,6 +33,26 @@ const uptadeElementYear = (elements) =>
       element.innerHTML = new Date().getFullYear();
    });
 
+const aboutsInteractive = (element, classCss) => {
+   const $element = element;
+   const $classCss = classCss;
+
+   $element.addEventListener('click', (e) => {
+      if (e.currentTarget === $element) {
+         $element.classList.toggle($classCss);
+      }
+   });
+
+   // add event for childrens
+   $element.querySelectorAll('*').forEach(child => {
+      child.addEventListener('click', (e) => {
+         if (e.currentTarget === $element) {
+            $element.classList.toggle($classCss);
+         }
+      });
+   });
+}
+
 addActiveAndClose(
    document.querySelector('.menu'),
    document.querySelectorAll('header i.fa-bars'),
@@ -45,5 +65,18 @@ uptadeElementYear(document.querySelectorAll('span.year'));
 clickResultClose(
    document.querySelectorAll('.menu nav li, .menu nav li a'),
    document.querySelector('.menu'),
+   'active'
+);
+
+aboutsInteractive(
+   document.querySelector('.about[data-order="1"]'),
+   'active'
+);
+aboutsInteractive(
+   document.querySelector('.about[data-order="2"]'),
+   'active'
+);
+aboutsInteractive(
+   document.querySelector('.about[data-order="3"]'),
    'active'
 );
