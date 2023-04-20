1b8bc7687dd3346817fc90142b81ec48e9ac2a6a
guilhermedevmatos15 <devguilhermeferreira@gmail.com>
2023-04-20 15:21:20 -0300

    feat: init animation on menu

--- assets/js/libs/initLibraries.js
+++ assets/js/libs/initLibraries.js
@@ -2,7 +2,7 @@ import initAOS from './aos.js';
 import initSmoothScroll from './smoothScroll.js';
 import initTypedTexts from './typed.js';
 
-initAOS(1800); // 1000 === 1 segundo
+initAOS(1400); // 1000 === 1 segundo
 initSmoothScroll(125, 2000);
 
 // ! Se não utilizar a library typed, deixe comentada para não gerar erros. Se quiser usar apenas descomente a linha abaixo e configure da maneira que quiser
